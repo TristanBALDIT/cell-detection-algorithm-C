6064e42604cfdfb8f5507234d8fe8f3700159c15
TristanBALDIT <tristan.baldit@gmail.com>
2025-09-17 14:55:29 +0200

    Initial Commit

---
+++ core.c
@@ -0,0 +1,291 @@
+#include "cbmp.h"
+#include "core.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define SAFE_IDX(x) ( (x) < 0 ? 0 : ((x) > 949 ? 949 : (x)) )
+
+
+static inline void set_gray_value (unsigned char pixel[3], unsigned char gray_value) {
+        pixel[0] =  pixel[1] = pixel[2] =  gray_value;
+}
+
+
+const int cross_mask[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+const int square_mask_3[8][2] = {{-1,0},{1,0},{0,-1},{0,1}, {-1,-1},{-1,1},{1,-1},{1,1}};
+const int square_mask_5[24][2] = {{-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2},
+                                {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2},
+                                {0, -2}, {0, -1}, {0, 1}, {0, 2},
+                                {1, -2}, {1, -1}, {1, 0}, {1, 1}, {1, 2},
+                                {2,-2}, {2, -1}, {2, 0}, {2, 1}, {2, 2}};
+const int square_mask_7[48][2] = {{-3,-3}, {-3, -2}, {-3, -1}, {-3, 0}, {-3, 1}, {-3, 2}, {-3, 3},
+                                {-2, -3}, {-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2}, {-2, 3},
+                                {-1, -3}, {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2}, {-1, 3},
+                                {0, -3}, {0, -2}, {0, -1}, {0, 1}, {0, 2}, {0, 3},
+                                {1, -3}, {1, -2}, {1, -1}, {1, 0}, {1, 1}, {1, 2}, {1,3},
+                                {2, -3}, {2, -2}, {2, -1}, {2, 0}, {2, 1}, {2, 2}, {2,3},
+                                {3, -3}, {3, -2}, {3, -1}, {3, 0}, {3, 1}, {3, 2}, {3, 3}};
+const int sphere_mask[36][2] = {{-3, -1}, {-3, 0}, {-3, 1},
+                                {-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2},
+                                {-1, -3}, {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2}, {-1, 3},
+                                {0, -3}, {0, -2}, {0, -1}, {0, 1}, {0, 2}, {0, 3},
+                                {1, -3}, {1, -2}, {1, -1}, {1, 0}, {1, 1}, {1, 2}, {1,3},
+                                {2, -2}, {2, -1}, {2, 0}, {2, 1}, {2, 2},
+                                {3, -1}, {3, 0}, {3, 1}};
+
+const int smooth_square_mask_7[44][2] = { {-3, -2}, {-3, -1}, {-3, 0}, {-3, 1}, {-3, 2},
+                                {-2, -3}, {-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2}, {-2, 3},
+                                {-1, -3}, {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2}, {-1, 3},
+                                {0, -3}, {0, -2}, {0, -1}, {0, 1}, {0, 2}, {0, 3},
+                                {1, -3}, {1, -2}, {1, -1}, {1, 0}, {1, 1}, {1, 2}, {1,3},
+                                {2, -3}, {2, -2}, {2, -1}, {2, 0}, {2, 1}, {2, 2}, {2,3},
+                                {3, -2}, {3, -1}, {3, 0}, {3, 1}, {3, 2}};
+const int smooth_square_mask_5[20][2] = {{-2, -1}, {-2, 0}, {-2, 1},
+                                {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2},
+                                {0, -2}, {0, -1}, {0, 1}, {0, 2},
+                                {1, -2}, {1, -1}, {1, 0}, {1, 1}, {1, 2},
+                                {2, -1}, {2, 0}, {2, 1}};
+
+const int sphere_mask_9[68][2] = {{-4, -2}, {-4, -1}, {-4, 0}, {-4, 1}, {-4, 2},
+                                {-3, -3}, {-3, -2}, {-3, -1}, {-3, 0}, {-3, 1}, {-3, 2}, {-3, 3},
+                                {-2, -4}, {-2, -3}, {-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2}, {-2, 3}, {-2, 4},
+                                {-1, -4}, {-1, -3}, {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2}, {-1, 3}, {-1, 4},
+                                {0, -4}, {0, -3}, {0, -2}, {0, -1}, {0, 1}, {0, 2}, {0, 3}, {0, 4},
+                                {1, -4}, {1, -3}, {1, -2}, {1, -1}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4},
+                                {2, -4}, {2, -3}, {2, -2}, {2, -1}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4},
+                                {3, -3}, {3, -2}, {3, -1}, {3, 0}, {3, 1}, {3, 2}, {3, 3},
+                                {4, -2}, {4, -1}, {4, 0}, {4, 1}, {4, 2}};
+
+typedef struct {
+    const int (*mask)[2];   // pointeur vers tableau [][2]
+    int size;               // nombre de points dans le masque
+} MaskDef;
+
+const MaskDef mask_list[] = {
+    { cross_mask, 4 },
+    { square_mask_3, 8 },
+    { square_mask_5, 24 },
+    {square_mask_7, 48},
+    {sphere_mask, 36},
+    {smooth_square_mask_5, 20},
+    {smooth_square_mask_7, 44},
+    {sphere_mask_9, 68}
+};
+
+int erosion_test_mask(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int i, int j, MaskDef m) {
+    for (int k = 0; k < m.size; k++) {
+        int y = SAFE_IDX(i + m.mask[k][0]);
+        int x = SAFE_IDX(j + m.mask[k][1]);
+        if (image[y][x][0] != 255) return 0;
+    } return 1;
+}
+
+static inline int cell_test(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int i, int j, int ws) {
+    int valid = 1;
+    int a = ws/2 + 1;
+    int b = ws/2;
+    int c = ws/2 -1;
+
+    //corner check
+    if (image[MAX(i-b, 0)][MAX(j-b,0)][0] == 255 ||
+        image[MIN(i+a, 949)][MAX(j-b,0)][0] == 255 ||
+        image[MAX(i-b,0)][MIN(j+a, 949)][0] == 255 ||
+        image[MIN(i+a, 949)][MIN(j+a, 949)][0] == 255) {
+        valid = 0;
+    }
+    else {
+        for (int x = 0; x < ws; x++) {
+            if (image[MAX(i-b, 0)][MIN(MAX(j-c+x, 0), 949)][0] == 255 ||
+                image[MIN(i+a, 949)][MIN(MAX(j-c+x, 0), 949)][0] == 255 ||
+                image[MIN(MAX(i-c+x, 0), 949)][MAX(j-b, 0)][0] == 255 ||
+                image[MIN(MAX(i-c+x, 0), 949)][MIN(j+a, 949)][0] == 255) {
+                valid = 0;
+                }
+        }
+    }
+
+    return valid;
+}
+
+void RGB2gray(unsigned char color_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char gray_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+    for (int i = 0; i < BMP_WIDTH; i++) {
+        for (int j = 0; j < BMP_HEIGTH; j++) {
+            unsigned char gray_value = (color_image[i][j][0] + color_image[i][j][1] + color_image[i][j][2])/3;
+            set_gray_value(gray_image[i][j], gray_value);
+        }
+    }
+}
+
+void gray2BW(unsigned char gray_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int threshold) {
+    for (int i = 0; i < BMP_WIDTH; i++) {
+        for (int j = 0; j < BMP_HEIGTH; j++) {
+            if (gray_image[i][j][0] > threshold) {
+                set_gray_value(gray_image[i][j], 255);
+            }
+            else {
+                set_gray_value(gray_image[i][j], 0);
+            }
+        }
+    }
+}
+
+int erosion(unsigned char (*src)[BMP_HEIGTH][BMP_CHANNELS], unsigned char (*dst)[BMP_HEIGTH][BMP_CHANNELS], int style) {
+    int fully_eroded = 1;
+    memset(dst, 0, BMP_WIDTH * BMP_HEIGTH * BMP_CHANNELS * sizeof(unsigned char));
+    for (int i = 1; i < BMP_WIDTH-1; i++) {
+        for (int j = 1; j < BMP_HEIGTH-1; j++) {
+            if (src[i][j][0] > 0 && erosion_test_mask(src, i, j, mask_list[style])) {
+                set_gray_value(dst[i][j], 255);
+                fully_eroded = 0;
+            }
+        }
+    }
+    return fully_eroded;
+}
+
+int detection(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int cells_center[MAX_CELLS][2], int nb_cells, int ws) {
+    for (int i = 1; i < BMP_WIDTH-1; i++) {
+        for (int j = 1; j < BMP_HEIGTH-1; j++) {
+            if (image[i][j][0] == 255 && cell_test(image, i, j, ws)) {
+                cells_center[nb_cells][0] = i;
+                cells_center[nb_cells][1] = j;
+                nb_cells++;
+                int a = ws/2 - 1;
+                for (int k = 0; k < ws; k++) {
+                    for (int l = 0; l < ws; l++) {
+                        if (i-a+k > 0 && i-a+k < 949 && j-a+l > 0 && j-a+l < 949) {
+                            set_gray_value(image[i-a+k][j-a+l], 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return nb_cells;
+}
+
+void generate_image(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char labelled_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int cells_center[MAX_CELLS][2], int nb_cells) {
+    for (int i = 0; i < BMP_WIDTH; i++) {
+        for (int j = 0; j < BMP_HEIGTH; j++) {
+            set_gray_value(labelled_image[i][j], image[i][j][0]);
+        }
+    }
+    for (int i = 0; i < nb_cells; i++) {
+        int x = cells_center[i][0];
+        int y = cells_center[i][1];
+
+        labelled_image[x][y][1] = 0;
+        labelled_image[x][y][2] = 0;
+        labelled_image[x+1][y][1] = 0;
+        labelled_image[x+1][y][2] = 0;
+        labelled_image[x][y+1][1] = 0;
+        labelled_image[x][y+1][2] = 0;
+        labelled_image[x+1][y+1][1] = 0;
+        labelled_image[x+1][y+1][2] = 0;
+
+        for (int j = 0; j < 16; j++) {
+
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y][0] = 255;
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y][1] = 0;
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y][2] = 0;
+
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y+1][0] = 255;
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y+1][1] = 0;
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y+1][2] = 0;
+
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y+1][0] = 255;
+            labelled_image[x][MIN(MAX(y-7 + j, 0),949)][1] = 0;
+            labelled_image[x][MIN(MAX(y-7 + j, 0),949)][2] = 0;
+
+            labelled_image[MIN(MAX(x-7 + j, 0),949)][y+1][0] = 255;
+            labelled_image[x+1][MIN(MAX(y-7 + j, 0),949)][1] = 0;
+            labelled_image[x+1][MIN(MAX(y-7 + j, 0),949)][2] = 0;
+        }
+    }
+}
+
+
+int main_algorithm(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int cells_center[MAX_CELLS][2], int nb_cells, int threshold) {
+
+    //Formatting the Input Image
+    RGB2gray(input_image, output_image);
+    gray2BW(output_image, threshold);
+    write_bitmap(output_image, "dbg.bmp");
+
+    static unsigned char eroded_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];     // Temp Eroded Image
+    unsigned char (*img_buffer[2])[BMP_HEIGTH][BMP_CHANNELS] = {output_image, eroded_image}; //Img swapping for erosion
+
+    int done;
+    int style = 6;      //First erosion pattern
+    int swap_count = 0;
+
+    do {
+
+        // Apply erosion
+        done = erosion(img_buffer[swap_count%2], img_buffer[(swap_count+1)%2], style); // returns 1 if fully eroded, 0 otherwise
+
+        //DEBUG
+        //char filename[64];
+        //sprintf(filename, "debug%d.bmp", nb_cells);
+        //write_bitmap(img_buffer[(swap_count+1)%2], filename);
+
+        style = 0;
+
+        if (!done) {
+            // Not fully eroded → run detection and generation
+            nb_cells = detection(img_buffer[(swap_count+1)%2], cells_center, nb_cells, 14);
+            swap_count++;
+        }
+    } while (!done);
+
+    generate_image(input_image, output_image, cells_center, nb_cells);
+    return nb_cells;
+}
+
+unsigned char otsu_method(unsigned char grey_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+    int histogram[256] = {0};
+    int total = BMP_WIDTH * BMP_HEIGTH;
+    int sum = 0;
+    for (int i = 0; i < BMP_WIDTH; i++) {
+        for (int j = 0; j < BMP_HEIGTH; j++) {
+            histogram[grey_image[i][j][0]]++;
+            sum += grey_image[i][j][0];
+        }
+    }
+
+    // 3️⃣ Otsu variables
+    long sumB = 0;      // sum of background
+    int wB = 0;         // weight of background
+    int wF = 0;         // weight of foreground
+    double maxVar = -1; // max between-class variance
+    unsigned char threshold = 0;
+
+    for (int t = 0; t < 256; t++) {
+        wB += histogram[t];            // background weight
+        if (wB == 0) continue;
+
+        wF = total - wB;                        // foreground weight
+        if (wF == 0) break;
+
+        sumB += t * histogram[t];                // background sum
+
+        double mB = (double)sumB / wB;           // background mean
+        double mF = (double)(sum - sumB) / wF;   // foreground mean
+
+        // Between-class variance
+        double varBetween = (double)wB * wF * (mB - mF) * (mB - mF);
+
+        if (varBetween > maxVar) {
+            maxVar = varBetween;
+            threshold = t;
+        }
+    }
+
+    return threshold;
+}
---
+++ core.h
@@ -0,0 +1,21 @@
+//
+// Created by trist on 10/09/2025.
+//
+
+#ifndef CORE_H
+#define CORE_H
+#include <stdbool.h>
+
+#define MAX_CELLS 1000
+
+void RGB2gray(unsigned char color_image[950][950][3], unsigned char gray_image[950][950][3]);
+void gray2BW(unsigned char gray_image[950][950][3], int threshold);
+int erosion(unsigned char (*src)[BMP_HEIGTH][BMP_CHANNELS], unsigned char (*dst)[BMP_HEIGTH][BMP_CHANNELS], int style);
+int detection(unsigned char image[950][950][3], int cells_center[MAX_CELLS][2], int nb_cells, int ws);
+void generate_image(unsigned char image[950][950][3], unsigned char labelled_image[950][950][3], int cells_center[MAX_CELLS][2], int nb_cells);
+int main_algorithm(unsigned char input_image[950][950][3], unsigned char output_image[950][950][3], int cells_center[MAX_CELLS][2], int nb_cells, int threshold);
+unsigned char otsu_method(unsigned char grey_image[950][950][3]);
+
+
+#endif //CORE_H
+
